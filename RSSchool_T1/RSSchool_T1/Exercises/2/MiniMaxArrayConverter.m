9ce194be2a5b329b78fea7e5dc8023b34952c2d8
dimonikus2020 <61157516+dimonikus2020@users.noreply.github.com>
2020-03-16 22:58:21 +0200

    feet: MinMaxSum completed

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,20 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    NSMutableArray *resultArray = [NSMutableArray new];
+    for (int i = 0; i < [array count]; i++) {
+        int sum = 0;
+        for (int x = 0; x < [array count]; x++) {
+            if (x != i) {
+                int current =[(NSNumber *)[array objectAtIndex:x] intValue];
+                sum = sum + current;
+            }
+        }
+        [resultArray addObject:@(sum)];
+    }
+    NSNumber *minSum=[resultArray valueForKeyPath:@"@min.intValue"];
+    NSNumber *maxSum=[resultArray valueForKeyPath:@"@max.intValue"];
+    return @[minSum,maxSum];
 }
 
 @end
