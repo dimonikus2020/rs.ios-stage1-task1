693e67aef68f9cd4d5062116bbcbd73cc89aa82f
dimonikus2020 <61157516+dimonikus2020@users.noreply.github.com>
2020-03-17 00:51:05 +0200

    feet: BillCounter complete

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,21 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    int jointBill = 0;
+    int annas = 0;
+    for (int i = 0; i < [bill count]; i++) {
+        if (i != index) {
+            jointBill = jointBill + [[bill objectAtIndex:i] intValue];
+        }
+    }
+    annas = jointBill/2;
+    if (annas != [sum intValue]) {
+        int result = [sum intValue] - annas;
+        return [NSString stringWithFormat:(@"%i"), result];
+    }
+    else {
+        return @"Bon Appetit";
+    }
 }
-
 @end
