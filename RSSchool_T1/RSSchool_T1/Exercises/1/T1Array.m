b94ca92fa1c2c1022a4a70a4655ca84ece156047
dimonikus2020 <61157516+dimonikus2020@users.noreply.github.com>
2020-03-16 22:18:00 +0200

    feet: complete T1array

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,32 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if (sadArray.count != 0) {
+        NSMutableArray *happyArray = [[NSMutableArray alloc] init];
+        [happyArray addObject: [sadArray firstObject]];
+        for (NSUInteger i = 1; i < sadArray.count-1; i++) {
+            int first = [[sadArray objectAtIndex:i-1] intValue];
+            int main = [[sadArray objectAtIndex:i] intValue];
+            int second = [[sadArray objectAtIndex:i+1] intValue];
+            if (main < (first + second)) {
+                [happyArray addObject: [sadArray objectAtIndex:i]];
+                for (NSUInteger i = 1; i < happyArray.count-1; i++) {
+                    int first = [[happyArray objectAtIndex:i-1] intValue];
+                    int main = [[happyArray objectAtIndex:i] intValue];
+                    int second = [[happyArray objectAtIndex:i+1] intValue];
+                    if (main > (first + second)) {
+                        [happyArray removeObjectAtIndex:i];
+                    }
+                }
+            }
+        }
+    } else {
+        NSArray *happyArray = [[NSArray alloc] init];
+        return happyArray;
+    }
+    [happyArray addObject: [sadArray lastObject]];
+    NSArray *happyArray_result = [[NSArray alloc] initWithArray:happyArray];
+    return happyArray_result;
 }
-
 @end
+
