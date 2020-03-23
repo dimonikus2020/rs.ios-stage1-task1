3c0a35e7fc4ba1e150afd0d1851a35c2852949b9
Admin <admin@192.168.32.129>
2020-03-23 14:05:29 +0300

    fix: resolve Task1 build issue

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,8 +4,9 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
+    NSMutableArray *happyArray = [[NSMutableArray alloc] init];
+
     if (sadArray.count != 0) {
-        NSMutableArray *happyArray = [[NSMutableArray alloc] init];
         [happyArray addObject: [sadArray firstObject]];
         for (NSUInteger i = 1; i < sadArray.count-1; i++) {
             int first = [[sadArray objectAtIndex:i-1] intValue];
@@ -14,7 +15,7 @@
             if (main < (first + second)) {
                 [happyArray addObject: [sadArray objectAtIndex:i]];
                 for (NSUInteger i = 1; i < happyArray.count-1; i++) {
-                    int first = [[happyArray objectAtIndex:i-1] intValue];
+    int first = [[happyArray objectAtIndex:i-1] intValue];
                     int main = [[happyArray objectAtIndex:i] intValue];
                     int second = [[happyArray objectAtIndex:i+1] intValue];
                     if (main > (first + second)) {
